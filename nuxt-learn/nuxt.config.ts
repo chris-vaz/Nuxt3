abdb4aa60cf5652cafa340c19f8f311e440b8273
chris-vaz <chrisvas2001@gmail.com>
2025-01-29 14:37:04 +0300

    Initial Commit

---
+++ nuxt-learn/nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
+export default defineNuxtConfig({
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true }
+})
