56496f196bf92fb3e9bffaa76ccc1df7161eef7b
chris-vaz <chrisvas2001@gmail.com>
2025-01-29 14:53:13 +0300

    Initialized Tailwind CSS and added Home.vue

--- nuxt-learn/nuxt.config.ts
+++ nuxt-learn/nuxt.config.ts
@@ -1,9 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   alias: {
     "@": resolve(__dirname, "/"),
   },
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
