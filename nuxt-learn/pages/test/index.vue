aee38918874308dfa24735393565bec85c76fbe9
chris-vaz <chrisvas2001@gmail.com>
2025-02-02 12:29:56 +0300

    Home shifted to index vue files

---
+++ nuxt-learn/pages/test/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="p-6 bg-white shadow-md rounded-lg">
+        <h1 class="text-3xl font-bold">Welcome to the Test Pages</h1>
+        <p class="text-lg text-gray-700 mt-4">Click on an ID to view dynamic content.</p>
+        <ul>
+            <li>
+                <NuxtLink to="/test/1">Go to Page with ID 1</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/test/2">Go to Page with ID 2</NuxtLink>
+            </li>
+        </ul>
+    </div>
+</template>
